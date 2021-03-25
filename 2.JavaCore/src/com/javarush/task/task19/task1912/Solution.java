d286f3611a53bc8f1c8e68e03268f02daeac07c0
Leohostname <l.t.shapoval@gmail.com>
2021-03-25 10:48:29 +0200

    task1912

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка 2
+*/
+
+public class Solution
+{
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException
+    {
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArray));
+
+        testString.printSomething();
+
+        System.setOut(consoleStream);
+
+        String line = byteArray.toString().replaceAll("te", "??");
+        System.out.println(line);
+
+        byteArray.close();
+    }
+
+    public static class TestString
+    {
+        public void printSomething()
+        {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
