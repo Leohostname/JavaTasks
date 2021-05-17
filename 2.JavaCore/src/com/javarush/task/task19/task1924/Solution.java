623ad4f0336e39df0634ea96e8549fd8b587eee6
Leohostname <l.t.shapoval@gmail.com>
2021-05-17 19:00:16 +0300

    task1924

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task19.task1924;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Замена чисел
+*/
+
+public class Solution
+{
+    public static Map<Integer, String> map = new HashMap<Integer, String>();
+
+    static
+    {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2, "два");
+        map.put(3, "три");
+        map.put(4, "четыре");
+        map.put(5, "пять");
+        map.put(6, "шесть");
+        map.put(7, "семь");
+        map.put(8, "восемь");
+        map.put(9, "девять");
+        map.put(10, "десять");
+        map.put(11, "одиннадцать");
+        map.put(12, "двенадцать");
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        reader.close();
+
+        FileReader fileReader = new FileReader(file);
+        String content = "";
+        while (fileReader.ready())
+        {
+            content += (char)fileReader.read();
+        }
+        fileReader.close();
+
+        for (Map.Entry<Integer, String> entry : map.entrySet())
+        {
+            content = content.replaceAll("\\b" + String.valueOf(entry.getKey()) + "\\b", entry.getValue());
+        }
+
+        System.out.println(content.trim());
+    }
+}
